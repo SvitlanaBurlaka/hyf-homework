37e4c23d698c4fd2656a5f9903a6aaf70f7f12c3
SvitlanaBurlaka <burlaka.sv.@gmail.com>
2022-02-04 11:41:50 +0100

    Filtering by price and rating added

--- javascript/javascript2/week2/homework app2/main.js
+++ javascript/javascript2/week2/homework app2/main.js
@@ -26,15 +26,40 @@ function renderProducts(ourProducts) {
 
 renderProducts(products);
 
-// Filter products
+// Filter products based on title
 
-const filterInputNames = document.querySelector(".searchingProducts");
+const filterInputTitle = document.querySelector(".filteringTitle");
 
-filterInputNames.addEventListener("input", function(e) {
-    const valueInput = e.target.value;
+filterInputTitle.addEventListener("input", function(e) {
+    const valueInputTitles = e.target.value;
     const filteredProducts = products.filter((item) =>
-        item.name.toLowerCase().includes(valueInput.toLowerCase())
+        item.name.toLowerCase().includes(valueInputTitles.toLowerCase())
     );
     console.log(filteredProducts);
     renderProducts(filteredProducts);
+});
+
+// Filter products based on max price
+
+const filterInputPrice = document.querySelector(".filteringPrice");
+
+filterInputPrice.addEventListener("input", function(e) {
+    const valueMaxPrice = e.target.value;
+    const filteredMaxPrices = products.filter(
+        (item) => item.price <= valueMaxPrice
+    );
+    filteredMaxPrices.sort((a, b) => a.price - b.price);
+    renderProducts(filteredMaxPrices);
+});
+
+// Filter products based on rating
+const filterInputRating = document.querySelector(".filteringRating");
+filterInputRating.addEventListener("input", function(e) {
+    const valueInputRating = e.target.value;
+    const filteredRating = products.filter(
+        (item) => item.rating <= valueInputRating
+    );
+    filteredRating.sort((a, b) => a.rating - b.rating);
+
+    renderProducts(filteredRating);
 });
