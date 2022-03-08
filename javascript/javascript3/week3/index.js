f070d0a059eebce03d950641a9fca49aa5897114
SvitlanaBurlaka <burlaka.sv.@gmail.com>
2022-03-08 11:37:12 +0100

    Changed part with circle

--- javascript/javascript3/week3/index.js
+++ javascript/javascript3/week3/index.js
@@ -2,37 +2,38 @@
 
 // Class creation time!
 class Circle {
-  constructor(x, y, r, startAngle, endAngle, fillColor) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.startAngle = startAngle;
-    this.endAngle = endAngle;
-    this.fillColor = fillColor;
-  }
-  draw() {
-    const canvas = document.getElementById("circle");
-    canvas.style.width = "100%";
-    canvas.style.height = "100%";
-    const ctx = canvas.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.r, this.startAngle, this.endAngle);
-    ctx.fillStyle = this.fillColor;
-    ctx.stroke();
-    ctx.fill();
-  }
+    constructor(x, y, r, startAngle, endAngle, fillColor) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.startAngle = startAngle;
+        this.endAngle = endAngle;
+        this.fillColor = fillColor;
+    }
+
+    draw(ctx) {
+        canvas.style.width = "100%";
+        canvas.style.height = "100%";
+
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, this.startAngle, this.endAngle);
+        ctx.fillStyle = this.fillColor;
+        ctx.stroke();
+        ctx.fill();
+    }
 }
 
 // const c1 = new Circle(50, 50, 20, 0, 2 * Math.PI, "yellow");
 // c1.draw();
-
+const canvas = document.getElementById("circle");
+const ctx = canvas.getContext("2d");
 // Now lets make art!
 setInterval(() => {
-  const xRandom = Math.floor(Math.random() * 500);
-  const yRandom = Math.floor(Math.random() * 500);
-  const rRandom = Math.floor(Math.random() * 200);
-  var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    const xRandom = Math.floor(Math.random() * 500);
+    const yRandom = Math.floor(Math.random() * 500);
+    const rRandom = Math.floor(Math.random() * 200);
+    var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 
-  const c1 = new Circle(xRandom, yRandom, rRandom, 0, 2 * Math.PI, randomColor);
-  c1.draw();
+    const c1 = new Circle(xRandom, yRandom, rRandom, 0, 2 * Math.PI, randomColor);
+    c1.draw(ctx);
 }, 100);
